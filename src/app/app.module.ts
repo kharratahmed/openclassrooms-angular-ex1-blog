fd028d44ae6c007e79db37b84b6b0c139f91b3fa
Kharrat Ahmed <ahmed.kharrat@intensy-t.com>
2019-08-11 03:45:25 +0100

    finish ex2 with firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,30 @@ import { NewPostComponent } from './new-post/new-post.component';
 import { ReactiveFormsModule } from '@angular/forms'
 import { PostService } from './services/post.service';
 import { PostListItemComponent } from './post-list-item-component/post-list-item.component';
-
+import * as firebase from 'firebase/app';
+import 'firebase/database'
 const appRoutes: Routes = [
   { path: 'posts', component: PostListComponent },
   { path: 'new', component: NewPostComponent },
   { path: '', redirectTo: "posts", pathMatch: 'full' },
   { path: '**', redirectTo: 'posts' }
 ]
+const firebaseConfig = {
+  apiKey: "AIzaSyBRAiA-nZ8eVDM4Fz_V2fWVcH3hLE6t5HU",
+  authDomain: "openclassroomsblogproject.firebaseapp.com",
+  databaseURL: "https://openclassroomsblogproject.firebaseio.com",
+  projectId: "openclassroomsblogproject",
+  storageBucket: "",
+  messagingSenderId: "396255813467",
+  appId: "1:396255813467:web:e94e4b6250e8b981"
+};
 
 @NgModule({
   declarations: [
     AppComponent,
     PostListComponent,
     PostListItemComponent,
-    NewPostComponent
+    NewPostComponent,
   ],
   imports: [
     BrowserModule,
@@ -30,4 +40,8 @@ const appRoutes: Routes = [
   providers: [PostService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor() {
+    firebase.initializeApp(firebaseConfig);
+  }
+}
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,36 +1,47 @@
-import { OnInit } from '@angular/core';
 import { Post } from '../models/Post.model';
 import { Subject } from 'rxjs';
-
-
+import * as firebase from 'firebase/app';
+import 'firebase/database'
 export class PostService {
 
-  private posts: Post[] = [
-    new Post("Mon premier post", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat. Duis semper.", 1),
-    new Post("mon deuxième post", "Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim. Pellentesque congue. Ut in risus volutpat libero pharetra tempor.", -1),
-    new Post("Encore un post", "Cras vestibulum bibendum augue. Praesent egestas leo in pede. Praesent blandit odio eu enim. Pellentesque sed dui ut augue blandit sodales. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam nibh. Mauris ac mauris sed pede pellentesque fermentum. Maecenas adipiscing ante non diam sodales hendrerit.", 0)
-  ];
+  private posts: Post[] = [];
   postsSubject = new Subject<Post[]>();
-  constructor() { }
+  constructor() {
+    this.getAllPostsFromFireBase();
+  }
   emitPostSubject() {
     this.postsSubject.next(this.posts.slice());
   }
+
+  getAllPostsFromFireBase() {
+    firebase.database().ref('/posts')
+      .on('value', (data: firebase.database.DataSnapshot) => {
+        this.posts = data.val() ? data.val() : [];
+        this.emitPostSubject();
+      }
+      );
+  }
+  saveAllPostsToFireBase() {
+    firebase.database().ref('/posts').set(this.posts);
+  }
   addPost(post: Post) {
     this.posts.push(post);
-    this.emitPostSubject();
+    this.saveAllPostsToFireBase();
+    //this.emitPostSubject();
   }
   removePost(post: Post) {
-    console.log(this.posts.length);
     this.posts.splice(this.posts.findIndex(item => item === post), 1);
-    console.log(this.posts.length);
-    this.emitPostSubject();
+    this.saveAllPostsToFireBase();
+    //this.emitPostSubject();
   }
   postLoveIt(post: Post) {
     this.posts[this.posts.findIndex(item => item === post)].loveIt += 1;
-    this.emitPostSubject();
+    this.saveAllPostsToFireBase();
+    //this.emitPostSubject();
   }
   postNotLoveIt(post: Post) {
     this.posts[this.posts.findIndex(item => item === post)].loveIt -= 1;
-    this.emitPostSubject();
+    this.saveAllPostsToFireBase();
+    //this.emitPostSubject();
   }
 }
